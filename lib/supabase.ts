464ac54e1dedafdff619b2313fc6741a9d9f3494
Jason Blackburn <blackburnjason1994@gmail.com>
2024-12-08 18:06:46 +0000

    adds environment variables to fix deployment error

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("supabaseUrl and supabaseKey are required");
+}
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
